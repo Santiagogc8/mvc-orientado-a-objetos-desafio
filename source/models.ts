911139158d4ce2a7ee72ee5e7f5c850c2fa35891
Santiago Guzman <guzmansantiago833@gmail.com>
2025-07-30 17:33:08 -0500

    30 Julio 2025

--- source/models.ts
+++ source/models.ts
@@ -1,6 +1,7 @@
 // este import existe solo para que tsc lo tome y lo copie a /build
-import "./contacts.json";
-import fs from "fs"; // Importamos fs
+require("./contacts.json");
+const fs = require("fs"); // Importamos fs
+const path = require("path"); // Importamos el path
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
 
@@ -10,12 +11,19 @@ class Contact {
 }
 
 class ContactsCollection { // Creamos la clase ContactsCollection
-  data: Contact[] = []; // Inicializamos data de tipo array de Contact y lo inicializamos vacio
+  private data: any[] = []; // Inicializamos data de tipo array y lo empezamos vacio
 
   load(){ // Creamos el metodo load
-    const obtenerData = fs.readFileSync('./contacts.json', 'utf-8'); // Que lee el archivo contacts.json con fs
-    const datos = JSON.parse(obtenerData); // Y luego, guardamos el json convertido en objeto en una variable
-    this.data = datos; // Le asignamos el objeto a this.data
+
+    const filePath = path.join(__dirname, "./contacts.json"); // Guardamos la direccion en la variable filePath
+
+    if (!fs.existsSync(filePath)) { // Si retorna false
+      throw new Error("El archivo contacts.json no existe."); // Mostramos un error
+    }
+
+    const obtenerData = fs.readFileSync(filePath, 'utf-8'); // Obtenemos la data con fs leyendo la ruta de filePath con utf-8
+    
+    this.data = JSON.parse(obtenerData); // E igualamos this.data al parse del JSON (obtenerData)
   }
 
   getAll(){ // Creamos el metodo que retorna todo el array de objetos
@@ -29,7 +37,8 @@ class ContactsCollection { // Creamos la clase ContactsCollection
 
   save(){ // Creamos el metodo save para guardar en el JSON los objetos agregados
     const dataToSave = JSON.stringify(this.data, null, 2); // Guardamos en la variable dataToSave la conversion de this.data, con valores null a reemplazar y con una identacion de 2 en el JSON
-    fs.writeFileSync('./contacts.json', dataToSave, 'utf-8'); // Y usamos el metodo writeFileSync para guardar en './contacts.json', la dataToSave, en formato utf-8
+    const filePath = path.join(__dirname, "contacts.json"); // Aseguramos que la ruta sea correcta
+    fs.writeFileSync(filePath, dataToSave, 'utf-8'); // Y usamos el metodo writeFileSync para guardar en './contacts.json', la dataToSave, en formato utf-8
   }
 
   getOneById(id: number){ // Creamos el metodo getOneById que recibe un id de tipo number
