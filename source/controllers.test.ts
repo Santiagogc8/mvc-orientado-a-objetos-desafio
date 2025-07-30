70f3f4898474a01fc31a2c4f002a827ab4af2ac6
Santiago Guzman <guzmansantiago833@gmail.com>
2025-07-30 17:05:24 -0500

    30 Julio 2025

--- source/controllers.test.ts
+++ source/controllers.test.ts
@@ -2,8 +2,46 @@ import test from "ava";
 import { ContactsController } from "./controllers";
 
 test("Testeo el constructor del controller", (t) => {
-  // test de ejemplo
-  t.truthy(true);
+	// test de ejemplo
+	t.truthy(true);
 });
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+	const contactController = new ContactsController();
+
+	const proceso1 = contactController.processOptions({
+		action: "get",
+		params: {
+			id: 1,
+			name: "Ana",
+		},
+	});
+
+	const proceso2 = contactController.processOptions({
+		action: "get",
+		params: {},
+	});
+
+	t.deepEqual(proceso1, { id: 1, name: "Ana" });
+
+	t.deepEqual(proceso2, [ 
+		{ id: 1, name: "Ana" },
+		{ id: 2, name: "Paula" },
+		{ id: 3, name: "Mer" },
+		{ id: 4, name: "Dana" },
+	]);
+
+  const proceso3 = contactController.processOptions({
+    action: "save",
+    params: {id: 5, name: "Santiago"}
+  });
+
+  t.deepEqual(proceso3, { message: "Contacto guardado" });
+
+  const proceso4 = contactController.processOptions({
+    action: null,
+    params: {}
+  });
+
+  t.deepEqual(proceso4, { message: "Acción no reconocida" })
+});
--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,19 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+  const args = minimist(argv.slice(2));
   return {
-    action: null,
-    params: null,
+    action: args.action || null, // Lee el args.action da null
+    params: args.params ? JSON.parse(args.params) : {}, // si params viene como string JSON lo parseamos
   };
 }
 
-function main() {}
+function main() {
+  const options = parseaParams(process.argv); // Utilizamos la funcion parseaParams y le pasamos el process.argv como parametros
+  const controller = new ContactsController(); // Instanciamos el controller
+  const result = controller.processOptions(options); // Y como resultado, utilizamos el metodo processOptions con los parametros parseados de options
+  console.log(result); // Imprimimos los resultados
+}
 
 main();
